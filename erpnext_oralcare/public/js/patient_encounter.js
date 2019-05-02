9bf69a297b8ee5abedcbdb8e80f6aa2a1dcfaec7
Palash Jhabak <palash@techlift.in>
2019-05-02 06:38:52 +0000

    Adding patient get details call

--- erpnext_oralcare/public/js/patient_encounter.js
+++ erpnext_oralcare/public/js/patient_encounter.js
@@ -1,5 +1,16 @@
 frappe.ui.form.on("Patient Encounter", "patient", function(frm){
 	console.log(frm)
+	if(frm.doc.patient){
+		frappe.call({
+			"method": "erpnext.healthcare.doctype.patient.patient.get_patient_detail",
+			args: {
+				patient: frm.doc.patient
+			},
+			callback: function (data) {
+				console.log(data.message)
+			}
+		});
+	}
 })
 
 frappe.ui.form.on("Patient Encounter", "onload", function(frm){
