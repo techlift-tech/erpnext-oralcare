3498202c41518c12b91698d2cf652111c35365fe
Palash Jhabak <palash@techlift.in>
2019-05-02 09:14:35 +0000

    Adding important medical data in the encounter form

--- erpnext_oralcare/public/js/patient_encounter.js
+++ erpnext_oralcare/public/js/patient_encounter.js
@@ -7,6 +7,18 @@ frappe.ui.form.on("Patient Encounter", "patient", function(frm){
 				patient: frm.doc.patient
 			},
 			callback: function (data) {
+				var blood_group = data.message.blood_group
+				var allergies = data.message.allergies
+				var medication = data.message.medication
+				var alchohol_current_use = data.message.alchohol_current_use
+				var alchohol_past_use = data.message.alchohol_past_use
+				var tobaco_current_use = data.message.tobaco_current_use
+				var tobaco_past_use = data.message.tobaco_past_use
+				var medical_history = data.message.medical_history
+				var surgical_history = data.message.surgical_history
+
+				frappe.mode.set_value(frm.doctype, frm.docname, "important_points", "<p>Palash</p>")
+
 				console.log(data.message)
 			}
 		});
