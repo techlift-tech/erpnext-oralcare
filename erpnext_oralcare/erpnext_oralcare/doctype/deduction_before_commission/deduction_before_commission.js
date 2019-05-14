1611ec3a0d23688bd5ca59a48304e4b73e27ae77
Palash Jhabak <palash@techlift.in>
2019-05-14 13:08:36 +0000

    Adding New Commission related doctypes

---
+++ erpnext_oralcare/erpnext_oralcare/doctype/deduction_before_commission/deduction_before_commission.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Techlift and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Deduction Before Commission', {
+	refresh: function(frm) {
+
+	}
+});
--- erpnext_oralcare/sales_invoice/sales_invoice_extend.py
+++ erpnext_oralcare/sales_invoice/sales_invoice_extend.py
@@ -1,32 +1,63 @@
 import frappe
 
+billable_healtcare_doctypes = ['Patient Appointment', 'Patient Encounter', 'Lab Test', 'Clinical Procedure', 'Procedure Prescription', 'Lab Prescription']
+
 def sales_invoice_doctors_share_calculate(doc, method):
 	sales_items = doc.items
 
 	for sales_item in sales_items:
 		reference_dt = sales_item.reference_dt
 		reference_dn = sales_item.reference_dn
+		item_code = sales_item.item_code
+		qty = sales_item.qty
+		amount = sales_item.amount
 
-		if(reference_dt == 'Clinical Procedure' and reference_dn):
+		if reference_dt in billable_healtcare_doctypes and reference_dt:
 
-			clinical_procedure = frappe.get_doc('Clinical Procedure', reference_dn)
-			if(clinical_procedure):
-				doctor = clinical_procedure.practitioner
+			healthcare_doc = frappe.get_doc(reference_dt, reference_dn)
 
-				if doctor:
-					price_list = frappe.get_list('Doctor Pricelist', filters = {'doctor_name': doctor, 'clinical_procedure': clinical_procedure.procedure_template}, fields = ['name', 'sharing_type', 'value'])
-
-					if (price_list and len(price_list) == 1):
-						amount = sales_item.amount
-						share_value = 0
-						qty = sales_item.qty
-
-						if price_list[0].sharing_type == 'percent':
-							share_value = (amount * price_list[0].value)/100
-
-						if price_list[0].sharing_type == 'fix':
-							share_value = qty*price_list[0].value
-
-						sales_item.doctor_share = share_value
+			if healthcare_doc:
+				if reference_dt == billable_healtcare_doctypes[0]:
+					break
+				elif reference_dt == billable_healtcare_doctypes[1]:
+					doctor = healthcare_doc.practitioner
+					sales_item.doctor_share = get_doctor_commission(item_code, doctor, qty, amount)
+					break
+				elif reference_dt == billable_healtcare_doctypes[2]:
+					break
+				elif reference_dt == billable_healtcare_doctypes[3]:
+					doctor = healthcare_doc.practitioner
+					sales_item.doctor_share = get_doctor_commission(item_code, doctor, qty, amount)
+					break
+				elif reference_dt == billable_healtcare_doctypes[4] or reference_dt == billable_healtcare_doctypes[5]:
+					encounter = healthcare_doc.parent
+					if encounter:
+						encounter_doc = frappe.get_doc(billable_healtcare_doctypes[1], encounter)
+						if encounter_doc:
+							doctor = encounter_doc.practitioner
+							sales_item.doctor_share = get_doctor_commission(item_code, doctor, qty, amount)
+					break
+				else:
+					break
 
 	return
+
+def get_doctor_commission(item_code, doctor, qty, amount):
+		if doctor:
+			commission_price = frappe.get_list('Item Commission List', filters = {'doctor_name': doctor, 'item_code': item_code})
+			share_value = 0
+
+			if len(commission_price) != 1:
+				return 0
+
+			commission_type = commission_price.commission_type
+			commission_value = commission_price.commission_value
+
+			if (commission_type and commission_value):
+				if commission_type == 'percent':
+					share_value = (amount * commission_value)/100
+				elif commission_type == 'fix':
+					share_value = qty*commission_value
+
+		return share_value
+
