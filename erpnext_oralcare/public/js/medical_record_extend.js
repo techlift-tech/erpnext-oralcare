a26aaa9f23b7a480e917c8ac7d36e1e16baf7ca4
Palash Jhabak <palash@techlift.in>
2019-05-30 07:05:04 +0000

    Fix: Fixing Medical Record Page
    Fixes: #15

--- erpnext_oralcare/public/js/medical_record_extend.js
+++ erpnext_oralcare/public/js/medical_record_extend.js
@@ -1,5 +1,5 @@
 var draw_page = function(patient, me){
-	console.log('test')
+	frappe.medical_record.last_feed_date = false;
 	frappe.model.with_doctype("Patient Medical Record", function() {
 		me.page.list = new MedicalView({
 			page_title: 'Medical Record',
@@ -10,7 +10,7 @@ var draw_page = function(patient, me){
 			method: 'erpnext.healthcare.page.medical_record.medical_record.get_feed',
 			args: {name: patient},
 			parent: $("<div></div>").appendTo(me.page.main),
-			show_filters: true,
+			show_filters: false,
 			doctype: "Patient Medical Record",
 			start: 0,
 			page_length: 20,
@@ -27,11 +27,12 @@ MedicalView = class MedicalView extends frappe.views.BaseList{
 	}
 
 	render(){
-		console.log('rrr')
 		var me = this;
 		var wrapper = me.page.main.find(".result-list").get(0);
+		frappe.medical_record.last_feed_date = false;
+		this.page.main.find("div.list-row").remove()
 		this.data.map(function (value) {
-			var row = $('<div class="list-row">').data("data", value).appendTo(me.$result).get(0);
+			var row = $('<div class="list-row" style="clear:both;">').data("data", value).appendTo(me.$result).get(0);
 			new frappe.medical_record.Feed(row, value);
 		});
 	}
@@ -53,5 +54,34 @@ MedicalView = class MedicalView extends frappe.views.BaseList{
 	setup_defaults() {
 		this.fields = []	
 		this.filters = []
+	}
+
+	setup_filter_area() {
+	}
+
+	setup_sort_selector() {
+	}
+
+	setup_list_wrapper() {
+		this.page.main.find("div.frappe-list").remove()	
+		this.$frappe_list = $('<div class="frappe-list">').appendTo(this.page.main);
+	}
+
+	refresh() {
+		//this.page.main.find("div.frappe-list").remove()	
+		//this.$frappe_list = $('<div class="frappe-list">').appendTo(this.page.main);
+		this.freeze(true);
+
+		return frappe.call(this.get_call_args()).then(r => {
+			this.prepare_data(r);
+			this.toggle_result_area();
+			this.before_render();
+			this.render();
+			this.after_render();
+			this.freeze(false);
+			if (this.settings && this.settings.refresh) {
+				this.settings.refresh(this);
+			}
+		});
 	}
 }
