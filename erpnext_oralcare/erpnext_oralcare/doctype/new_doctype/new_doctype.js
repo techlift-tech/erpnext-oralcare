12c7696038b7b8db5c4f2b1ebc0efaeeec2c6008
Palash Jhabak <palash@techlift.in>
2019-06-03 08:58:35 +0000

    removing new doctype

--- erpnext_oralcare/erpnext_oralcare/doctype/new_doctype/new_doctype.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2019, Techlift and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('new doctype', {
-	// refresh: function(frm) {
-
-	// }
-});
