a2822883f2b05bdaf74856a63ef07ff62d93427b
Palash Jhabak <palash@techlift.in>
2019-07-07 05:35:11 +0000

    Feat: Adding family in customer doctype
    Fixes: #17

--- erpnext_oralcare/patient/patient.py
+++ erpnext_oralcare/patient/patient.py
@@ -11,3 +11,13 @@ def send_referrer_sms(doc, method):
 		message = frappe.render_template(referrer_sms_message, context)
 		numbers = [referrer_phone_no]
 		send_sms(numbers, message)
+
+def save_family_name(doc, method):
+	family_name = doc.family_name
+	customer = doc.customer
+
+	if family_name and customer:
+		customer_doc = frappe.get_doc('Customer', customer)
+		if customer_doc:
+			customer_doc.family_name = family_name
+			customer_doc.save()
