d29dbf3812bb4b791a0401464f8d9dc5b81a4aa0
Your Name <varade.suraj787@gmail.com>
2019-12-11 16:57:08 +0530

    missing json

--- erpnext_oralcare/erpnext_oralcare/report/doctor_financial_report_query/doctor_financial_report_query.js
+++ erpnext_oralcare/erpnext_oralcare/report/doctor_financial_report_query/doctor_financial_report_query.js
@@ -2,8 +2,25 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Doctor Financial report Query"] = {
+frappe.query_reports["Doctor Financial Report Script"] = {
 	"filters": [
+		{
+			fieldname: "practitioner",
+			label: __("Doctor"),
+			fieldtype: "Data"
+		},
+		{
+			fieldname:"start_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			width: "80"
+		},
+		{
+			fieldname:"end_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			width: "80"
+		}
 
 	]
 }
