b1242798688b65e7c88ee90c926ba2cdbafe8cf9
surajTechlift <49982705+surajTechlift@users.noreply.github.com>
2020-01-13 15:44:54 +0530

    final individual doctor data sheet report

--- erpnext_oralcare/erpnext_oralcare/report/individual_doctor_data_sheet_query/individual_doctor_data_sheet_query.py
+++ erpnext_oralcare/erpnext_oralcare/report/individual_doctor_data_sheet_query/individual_doctor_data_sheet_query.py
@@ -74,27 +74,26 @@ def get_columns(filters=None):
 
 		
 def prepare_data(filters):
-	cond = cond2 = ""
+	date_cond = practitioner_cond = ""
 	if filters.practitioner:
-		cond = "having practitioner='{0}'".format(filters.get('practitioner'))
+		practitioner_cond = "and practitioner='{0}'".format(filters.get('practitioner'))
 	if filters.start_date and filters.end_date:
-		cond2 = "where si.posting_date BETWEEN '{0}' AND '{1}'".format(filters.start_date, filters.end_date)	
+		date_cond  = "where si.posting_date BETWEEN '{0}' AND '{1}'".format(filters.start_date, filters.end_date)	
 	query = """
 	select
 	si.name as "name",
-	DATE_FORMAT(si.posting_date,'%m-%d-%Y') as "posting_date",
+	DATE_FORMAT(si.posting_date,'%d-%m-%Y') as "posting_date",
 	si_item.item_name as "item_name",
 	si.customer_name as "patient_name",
 	case si_item.reference_dt when "Clinical Procedure" 
-								then (select practitioner from `tabClinical Procedure` where name = si_item.reference_dn)
-								when "Patient Appointment" 
-								then (select practitioner from `tabPatient Appointment` where name = si_item.reference_dn)
-								 END as "practitioner",
+	then (select practitioner from `tabClinical Procedure` where name = si_item.reference_dn)
+	when "Patient Appointment" 
+	then (select practitioner from `tabPatient Appointment` where name = si_item.reference_dn)
+	END as "practitioner",
 	ifnull(si_item.doctor_share,0) as "doctor_share",
 	si_item.amount as "amount",ifnull(si_item.amount-si_item.doctor_share,0) as "value_to_company" 
-	from `tabSales Invoice`as si inner JOIN `tabSales Invoice Item`as si_item on si.name=si_item.parent {1}{0} having practitioner is not null;""".format(cond,cond2)
+	from `tabSales Invoice`as si inner JOIN `tabSales Invoice Item`as si_item on si.name=si_item.parent and si.docstatus=1 {0} having practitioner is not null {1};""".format(date_cond,practitioner_cond)
 
-	frappe.msgprint(query)
 	data = frappe.db.sql(query,as_dict=True)
 	
 	return data
